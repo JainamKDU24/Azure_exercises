c770c417aac759a4798b148009f6f029e676becd
JainamKDU24 <jainamrajiv.gandhi@kickdrumtech.com>
2024-01-12 22:58:56 +0530

    Exercise 2

---
+++ src/main/java/com/kdu/exercise2/Exercise2Application.java
@@ -0,0 +1,22 @@
+package com.kdu.exercise2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+public class Exercise2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Exercise2Application.class, args);
+	}
+	@RestController
+	class HelloController {
+		@GetMapping("/")
+		public String index() {
+			return "Hello Jainam,KD-24 batch (CLI)";
+		}
+	}
+
+}
